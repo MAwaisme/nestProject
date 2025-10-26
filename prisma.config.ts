bb8876d50de57afe32371532590e2fc4e7a0930a
Muhammad Awais <muhammad.awais.genius@gmail.com>
2025-10-26 19:36:51 +0500

    issues

---
+++ prisma.config.ts
@@ -0,0 +1,14 @@
+// Load environment variables from .env
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  engine: "classic",
+  datasource: {
+    url: env("DATABASE_URL"), // now this will correctly read from .env
+  },
+});
