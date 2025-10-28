26ec0f48fd9c5ec0624250d8691a6be105a24438
Muhammad Awais <muhammad.awais.genius@gmail.com>
2025-10-28 14:04:42 +0500

    simple JWT added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, BookmarkModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    AuthModule,
+    PrismaModule,
+    BookmarkModule,
+    UserModule
+  ],
 })
-export class AppModule {}
+export class AppModule { }
