d6b8c3cd5ea519433fb51e9db721b79dbdc96ce3
Muhammad Awais <muhammad.awais.genius@gmail.com>
2025-11-10 11:29:20 +0500

    asd

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, UseGuards, Req } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
+import { User } from 'generated/prisma';
+import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
 
 @Controller('users')
@@ -8,7 +10,11 @@ export class UsersController {
     @UseGuards(JwtGuard)
     @Get('me')
 
-    getProfile(@Req() req: Request) {
-        return req.user; // typed correctly now
+    // getProfile(@Req() req: Request) {
+    //     return req.user; // typed correctly now
+    // }
+
+    getMe(@GetUser() user: User) {
+        return user
     }
 }
